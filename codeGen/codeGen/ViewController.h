f79f92d8b63a73d85b75879d67a47a8b1551469b
wangjianfei <hzwangjianfei@corp.netease.com>
2020-09-15 18:56:47 +0800

    删除误加的

--- codeGen/codeGen/ViewController.h
+++ codeGen/codeGen/ViewController.h
@@ -10,21 +10,5 @@
 
 @interface ViewController : UIViewController
 
-@property (nonatomic, assign) NSInteger liveId;
-@property (nonatomic, assign) NSInteger status;
-@property (nonatomic, assign) NSInteger countdownTime;
-@property (nonatomic, assign) BOOL subscribed;
-@property (nonatomic, copy) NSString *title;
-@property (nonatomic, copy) NSString *bgPic;
-@property (nonatomic, copy) NSString *coverPic;
-@property (nonatomic, copy) NSString *coverSubPic;
-@property (nonatomic, copy) NSArray *pullUrls;
-@property (nonatomic, copy) NSString *buyingDes;
-@property (nonatomic, copy) NSString *warnings;
-@property (nonatomic, strong) <#Class#> shareInfo;
-@property (nonatomic, assign) NSInteger viewCount;
-@property (nonatomic, strong) <#Class#> anchorInfo;
-@property (nonatomic, strong) <#Class#> livePasterBean;
-
 @end
 
